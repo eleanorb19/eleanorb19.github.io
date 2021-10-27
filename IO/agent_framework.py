43bb26ba6fef187f5b87f21832c234cc54f87378
eleanorb19 <90636185+eleanorb19@users.noreply.github.com>
2021-10-27 14:18:56 +0100

    Update agent_framework.py

--- IO/agent_framework.py
+++ IO/agent_framework.py
@@ -14,8 +14,8 @@ class Agent():
         Parameters
         ----------
         environment : List holding environment data
-        rows : Length of environment
-        cols : Length of environment
+        rows : the length of the environment
+        cols : the length of the first row of the environment
         i : iteration number
 
         """
@@ -28,7 +28,8 @@ class Agent():
         self.cols = cols
         self.i = i
     
-    # Printing and naming the agents so that they display information about their location and stores
+    # Printing and naming the agents so that they display information about 
+    # their location and stores
     def __str__(self):
         """
         Returns
@@ -54,11 +55,13 @@ class Agent():
         """
         Creating a move method within the Agent class
         If statement against a random number between 0 and 1.
-        If the number is less than 0.5 then the x or y coordinate will increase by 1
+        If the number is less than 0.5 then the x or y coordinate will 
+        increase by 1
         and vice versa for greater than 0.5. 
         """
         if random.random() < 0.5:
-            self._y = (self._y + 1) % self.rows # Ensuring agents can wander around full environment
+            self._y = (self._y + 1) % self.rows # Ensuring agents can wander 
+            # around full environment
         else:
             self._y = (self._y - 1) % self.rows
 
@@ -82,12 +85,14 @@ class Agent():
             self.environment[self._y][self._x] = 0
             # print("After eat", self.environment[self._y][self._x])
             # print(self)
-        # Agents sick up their store in a location if they've eaten more than 100 units
+        # Agents sick up their store in a location if they've eaten more 
+        # than 100 units
         if self.store > 100:
             self.environment[self._y][self._x] += 100
             self.store = 0
             #print("Sick")
-            
+    
+    # Calculating distance between agents 
     def distance_between(self, b):
         """
         Finds pythagorean distance between two agents
